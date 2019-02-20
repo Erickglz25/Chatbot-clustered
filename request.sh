f0966883217f8e50276ce15d055fa3fde11e478a
VZERT14 <user@ejemplo.com>
2019-02-19 18:58:38 -0600

    First commit/ code review for production

---
+++ request.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# chmode +x ./file make it executable
+bash -c 'for i in `seq 1 1000`; 
+do 
+curl -v -d "UserMsg=hey" -d "UserName=Hosting1A" -d "AccessToken=iusybe87ybc88um9m8uxw" http://localhost:3000/send-messages
+done'
+
---
+++ worker.js
@@ -0,0 +1,171 @@
+var SCWorker = require('socketcluster/scworker');
+var express = require('express');
+var healthChecker = require('sc-framework-health-check');
+const bodyParser = require('body-parser');
+const apiai = require('apiai');
+const mongoose = require('mongoose');
+const Agent = require('./models/agent');
+const Promise = require('promise');
+
+require('dotenv').config();
+const config = require('./config/config');
+
+let AgentNameLocal = process.env.A_NAME; //variables, con valores arbitrarios complejos para evitar 'undefined'  
+let AgentTokenLocal = process.env.A_TOKEN_LOCAL;//no representan ninguna vulnerabilidad
+let AgentSessionLocal = process.env.A_SESSION_LOCAL;
+
+mongoose.set('useCreateIndex', true)    
+mongoose.connect(process.env.MONGODB_URI,{ useNewUrlParser: true }); 
+
+const db = mongoose.connection; 
+
+db.on('error', console.log.bind(console, 'connection error:'));
+
+db.once('open', function() {                
+  console.log('Connection Success');        
+})
+
+
+class Worker extends SCWorker {             
+  run() {
+    console.log('   >> Worker PID:', process.pid); 
+    var environment = this.options.environment; //linea verificada set now = 'produccion'
+
+    var app = express();       
+
+    var httpServer = this.httpServer;   
+    var scServer = this.scServer;       
+
+    // Add GET /health-check express route
+    healthChecker.attach(this, app);           
+
+    httpServer.on('request', app);             
+    
+    /*
+      In here we handle our incoming realtime connections and listen for events.
+    */
+
+   app.use(bodyParser.json());            
+   
+   app.use(bodyParser.urlencoded({ extended: true }));         
+
+   app.post('/send-messages',config._checkToken,config._reviewBasics,function(req,res,next) {
+      
+      if( AgentNameLocal === req.body.UserName ){
+          
+          function getDialogFlow(){  
+              return new Promise((res,rej) =>{
+  
+                  var request = apiai(AgentTokenLocal).textRequest(req.body.UserMsg, {
+                      sessionId: req.body.UserName+'session'
+                  });
+                  res(request);
+                  
+              })
+          }
+      
+          getDialogFlow()
+
+              .then((request)=>{
+          
+                  request.on('response', function(response) { 
+                      
+                      if (response.result.metadata.isFallbackIntent === 'true'  || response.result.metadata.intentName === 'support.problem')
+                          return res.status(201).json("Bot Asking For Help"); 
+          
+                      return res.status(200).json(response.result.fulfillment['speech']);
+                  });
+              
+                  request.on('error', function(error) {
+                      return res.status(501).json(error);
+                  });
+              
+                  request.end();
+              })
+              .catch((err) => {
+                  return res.status(403).json(err);
+              });
+
+      }else{
+                  
+          function SaveAgent(){  
+              return new Promise((res,rej) =>{
+
+                  Agent.findOne({agentName: req.body.UserName},function(error,myagent){
+                      
+                      if(error) rej(error);
+                      if(myagent == null) rej('agent not found');
+          
+                      res(myagent);     
+                  }); 
+              })
+          }
+          
+          SaveAgent()
+              .then((myagent) => {
+          
+                  AgentNameLocal = myagent.agentName;
+                  AgentSessionLocal = myagent.agentSession;
+                  AgentTokenLocal = myagent.agentToken;
+
+                  let dialogflow = apiai(myagent.agentToken);
+                  return dialogflow;
+
+              })
+              .then((dialogflow)=>{
+                  var request = dialogflow.textRequest(req.body.UserMsg, {
+                      sessionId: req.body.UserName+'session' || 'M4ND78ND'
+                  });
+                  return request;
+              })
+              .then((request)=>{
+          
+                  request.on('response', function(response) {     
+                          
+                      if (response.result.metadata.isFallbackIntent === 'true'  || response.result.metadata.intentName === 'support.problem')
+                          return res.status(201).json("Bot Asking For Help"); 
+
+                      return res.status(200).json(response.result.fulfillment['speech']);
+                  });
+              
+                  request.on('error', function(error) {
+                      return res.status(500).json(error);
+                  });
+              
+                  request.end();
+          
+              })
+              .catch((err) => {
+                  return res.status(403).json(err);
+              });
+      }
+
+  });                                
+  // -_____________________________----------------------------------------- Todas las lineas verificadas hasta este punto -----------------------------------------------
+  app.post('/create-agent',config._checkToken,function(req,res) { 
+      
+      var agent = new Agent({
+          agentName: req.body.Name,
+          agentToken: req.body.ClientToken,
+          agentStatus: true,
+          agentSession: req.body.Name+'Session'
+      });
+
+      return new Promise((res,rej) =>{
+          agent.save(function(err,result){
+              if(err) rej(err);
+              res();     
+          }); 
+      }).then(()=>{
+          return res.status(200).json('Agent succesfully created inside a promise');
+      })
+      .catch((err) => {
+        return res.status(403).json(err);
+      });
+
+  });
+
+  }
+}
+
+new Worker();
